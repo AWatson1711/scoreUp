3d736243030d66e67f7fd488daa38303e30972d9
AWatson1711 <a.watson1711@gmail.com>
2023-02-24 13:50:08 +0100

    first commit

---
+++ react/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Suivis Records</h1>
+      <Nav />
+    </header>
+  );
+};
+
+export default Header;
